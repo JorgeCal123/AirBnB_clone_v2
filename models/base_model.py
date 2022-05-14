4598edb8e20b9a479681f8cf608ce0c0a6c6045a
JorgeCal123/boom.git <git@github.com>
2022-05-14 08:30:09 -0700

    commit jorge

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """chnage basemol by Jorge"""
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
     updated_at = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
