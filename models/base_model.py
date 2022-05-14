ae10879c3de56ee71266091238725dde0ccc7bcc
jhonpulido1990 <3825@holbertonschool.com>
2022-05-14 10:24:15 -0500

    change datatime in base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -23,9 +23,9 @@ class BaseModel:
             self.updated_at = datetime.datetime.now()
 
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            kwargs['updated_at'] = datetime.datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            kwargs['created_at'] = datetime.datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
