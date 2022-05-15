5d8c207e2e19b0e0dbd130019a9e20e5abfa856f
JorgeCal123/boom.git <git@github.com>
2022-05-15 12:49:20 -0700

    correction datetime

--- models/base_model.py
+++ models/base_model.py
@@ -24,14 +24,15 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
+            
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
-
+            storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            kwargs['updated_at'] = datetime.datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            kwargs['created_at'] = datetime.datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
@@ -45,7 +46,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.datetime.now()
         storage.new(self)
         storage.save()
 
