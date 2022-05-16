fe6769e13aaaf258bd774e513cac1c177028df05
jhonpulido1990 <3825@holbertonschool.com>
2022-05-16 15:23:20 -0500

    update amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy.orm import relationship
+import sqlalchemy
+from sqlalchemy import Column, ForeignKey, String
 
+class Amenity(BaseModel, Base):
 
-class Amenity(BaseModel):
-    name = ""
+    __tablename__ = "amenities"
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+    
