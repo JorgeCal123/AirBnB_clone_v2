f14deeb8e6d1599d6b600495312c951f4c5fab2c
jhonpulido1990 <3825@holbertonschool.com>
2022-05-16 10:14:16 -0500

    change conditionalof place

--- models/place.py
+++ models/place.py
@@ -9,8 +9,10 @@ from sqlalchemy.orm import relationship
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    if models.storage_t == 'db':
-        __tablename__ = 'places'
+
+    __tablename__ = 'places'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
