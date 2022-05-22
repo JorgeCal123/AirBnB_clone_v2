c9cad5f80612fc80d8c4b39ab279d3b796af9400
JorgeCal123/boom.git <git@github.com>
2022-05-21 17:33:30 -0700

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,19 @@
 #!/usr/bin/env bash
 # Configure your Nginx server
 
-if [ ! -x /usr/sbin/nginx ];
-then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-    sudo ufw allow  'Nginx HTTP' #on port 80
-    sudo service nginx restart
-else
-
-    sudo service nginx restart
-fi
-sudo mkdir -p data/web_static/releases/test
-sudo touch  data/web_static/releases/test/index.html
-echo "<html>
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" >> data/web_static/releases/test/index.html
-sudo mkdir -p data/web_static/current
-
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu:ubuntu data
-sudo sed -i "5i rewrite \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;/\n\t}" /etc/nginx/sites-available/default
+sudo chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
